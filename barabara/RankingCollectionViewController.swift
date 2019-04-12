b0bc949fab2ce918e39684301a1a5dc773ac5153
appleEmily <emiri3128@gmail.com>
2019-04-12 22:36:10 +0900

    not move:(

--- barabara/RankingCollectionViewController.swift
+++ barabara/RankingCollectionViewController.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-class RankingCollectionViewController: UICollectionViewController {
+class RankingCollectionViewController: UIViewController {
     
+    
+    @IBOutlet weak var rankingLabel1: UILabel!
+    
+    @IBOutlet weak var rankingLabel2: UILabel!
+    
+    @IBOutlet weak var rankingLabel3: UILabel!
+    
+    let defaults: UserDefaults = UserDefaults.standard
+    
+    @IBAction func back() {
+        self.dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        rankingLabel1.text = String(defaults.integer(forKey: "score1"))
+        rankingLabel2.text = String(defaults.integer(forKey: "score2"))
+        rankingLabel3.text = String(defaults.integer(forKey: "score3"))
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
--- barabara/ViewController.swift
+++ barabara/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBAction func start() {
+    }
+    
+    
+    @IBAction func ranking() {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
